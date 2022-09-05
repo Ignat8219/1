627bc70b0fad79e70aaee93ff46b7eee46309eb2
Ignat8219 <107505649+Ignat8219@users.noreply.github.com>
2022-09-05 21:30:12 +0300

    Add files via upload

---
+++ HW/config.py
@@ -0,0 +1,14 @@
+TOKEN = '5605221925:AAHV2BaEYBKhwwNTC0oIEuOX62WqwRzCeA0'
+keys ={
+    'bitcoin': 'BTC',
+    'ethereum': 'ETH',
+    'solana': 'SOL',
+    'tether': 'USDT',
+    'xrp': 'XRP',
+    'usd': 'USD',
+    'jpy': 'JPY',
+    'eur': 'EUR',
+    'руб': 'RUB',
+    'bat': 'BAT',
+    'юань': 'CNY',
+}
